c5df9329d5e238ae807a14134823731dfb10f51f
Luis Gonzalez <luisgnzhdz@gmail.com>
2025-05-12 23:55:26 -0600

    [FEAT]: Se agrego la funcion de agregar usuarios
    -Se creo la pagina de agregar usuario\n -Se actualizo el backend para tener una buena comunicacion con la nueva pagina agregada\n -Se organizaron los archivos para un mejor mantenimiento

--- src/main/java/gm/empleados/controlador/IndexControlador.java
+++ src/main/java/gm/empleados/controlador/IndexControlador.java
@@ -2,16 +2,22 @@ package gm.empleados.controlador;
 
 import gm.empleados.modelo.Empleado;
 import gm.empleados.servicios.EmpleadoServicio;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class IndexControlador {
@@ -28,5 +34,17 @@ public class IndexControlador {
         // Compartimos el modelo de la vista
         modelo.put("empleados", empleados);
         return "index"; //index.jsp
+    }
+
+    @RequestMapping(value="/agregar", method=RequestMethod.GET)
+    public String mostrarAgregar() {
+        return "agregar";
+    }
+
+    @RequestMapping(value="/agregar", method=RequestMethod.POST)
+    public String agregar(@ModelAttribute("empleadoForma") Empleado empleado){
+        logger.info("Empleado a agregar: " + empleado);
+        empleadoServicio.guardarEmpleado(empleado);
+        return "redirect:/";
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/comunes/cabecero.jsp
@@ -0,0 +1,15 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
+      crossorigin="anonymous"
+    />
+    <title>Sistema de Empleados</title>
+  </head>
+  <body>
